3accf5db7813f954cd637109b54e0ef0836b3449
Ayush Patel <ayushap05@gmail.com>
2023-10-23 22:58:32 +0530

    Changed Folder Structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-const geocode = require('../utils/geocode'); 
-const weather = require('../utils/weather'); 
+const geocode = require('./utils/geocode'); 
+const weather = require('./utils/weather'); 
 
 
 
